0913eda645af832e6ea74914b6dd8b0857d01800
Luca <mrmarkolinus@gmail.com>
2018-01-19 00:17:01 +0100

    -risolto bug del pulsante indietro -sostituito timer nella schermata di regole con un bottone

--- app/src/main/java/com/example/luca/abilitybuttun/MainActivity.java
+++ app/src/main/java/com/example/luca/abilitybuttun/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+/*per il bottone indietro*/
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    /*per gestire il bottone indietro*/
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    /*fine sezione bottone indietro*/
 
 }
--- app/src/main/java/com/example/luca/abilitybuttun/firstActivity.java
+++ app/src/main/java/com/example/luca/abilitybuttun/firstActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+/*per il bottone indietro*/
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 import java.util.Random;
 import java.util.*;
@@ -258,5 +261,30 @@ public class firstActivity extends AppCompatActivity {
         timer.start();
 
     }
+
+    /*per gestire il bottone indietro*/
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    /*fine sezione bottone indietro*/
 }
 
