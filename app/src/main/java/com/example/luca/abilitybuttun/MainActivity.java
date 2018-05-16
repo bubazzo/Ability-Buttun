bac6f696de66677518c45ff07e21af42f76e56bb
CapCandy <32848098+CapCandy@users.noreply.github.com>
2018-05-16 13:40:00 +0200

    Aggiunta musica da migliorare

--- app/src/main/java/com/example/luca/abilitybuttun/MainActivity.java
+++ app/src/main/java/com/example/luca/abilitybuttun/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.luca.abilitybuttun;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.media.Image;
+import android.os.Binder;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +21,45 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Integer lvRaggiunto=0;
 
+
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon,Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MusicService mServ = new MusicService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        startService(music);
         TextView testoLv=(TextView) findViewById(R.id.textLivello);
         Button start=(Button) findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
@@ -73,4 +112,5 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
+
 }
